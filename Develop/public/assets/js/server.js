3a24f29e856fe923e067b3808ce83711e2d40b56
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-05-21 21:15:00 +1000

    All routes added. Error in index.js

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -22,9 +22,14 @@ app.use(express.json());
 app.use(express.static("./Develop/public"));
 
 //Api route "GET request"
-app.get("/", (req, res) => {
-   res.send("Hello");
-});
+// app.get("/", (req, res) => {
+//    res.send("Hello"); //index.html
+// });
+
+//Api route "GET request"
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, '../../notes.html')); //index.html
+  });
 
 //Api route "GET request"
 app.get("/api/notes", (req, res) => {
@@ -49,6 +54,39 @@ app.post("/api/notes", (req, res) => {
         res.json(note);
     })
 });
+
+// Api route "Delete" request
+app.delete("/api/notes/:id", (req, res) => {
+    const idToDelete = parseInt(req.params.id);
+    readFileAsync("../db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        const newNotesData = []
+        for (let i = 0; i < notes.length; i++) {
+            if(idToDelete !== notes[i].id) {
+                newNotesData.push(notes[i]);
+            }
+        }
+        return newNotesData;
+    }).then(function(notes) {
+        writeFileAsync("../db/db.json", JSON.stringify(notes))
+        res.send("Note Saved!");
+    })
+});
+
+// html routes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../notes.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../../index.html')); //index.html
+  });
+ 
+
+//Api route "GET request"
+app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, '../../index.html')); //index.html
+ });
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
